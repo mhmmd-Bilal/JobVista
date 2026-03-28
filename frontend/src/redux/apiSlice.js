3b55534382a44cdc368132ef31ea0315def4ff7a
Muhammed Bilal <130375693+mhmmd-Bilal@users.noreply.github.com>
2026-03-28 18:00:13 +0530

    Update apiSlice.js

--- frontend/src/redux/apiSlice.js
+++ frontend/src/redux/apiSlice.js
@@ -1,6 +1,6 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react'
 
-const baseQuery = fetchBaseQuery({ baseUrl: '' })
+const baseQuery = fetchBaseQuery({ baseUrl: 'https://jobvista-backend-f2zl.onrender.com' })
 
 export const userApiSlice = createApi({
   baseQuery,
